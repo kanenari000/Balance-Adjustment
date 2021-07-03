dbe32a79db38c50a420211d98cd4a7aa5003fbd0
kanenari000 <79363686+kanenari000@users.noreply.github.com>
2021-07-03 12:25:20 +0000

    Deploying to gh-pages from  @ 60b5b5a27ceae9266e7a46cbc39da271e45d507a 🚀

---
+++ _nuxt/static/1625315115/battle/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FBalance-Adjustment\u002F_nuxt\u002Fstatic\u002F1625315115",layout:"default",error:a,serverRendered:true,routePath:"\u002Fbattle",config:{_app:{basePath:"\u002FBalance-Adjustment\u002F",assetsPath:"\u002FBalance-Adjustment\u002F_nuxt\u002F",cdnURL:a}}}}(null));
